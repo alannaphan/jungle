3e7004751c7bea5e97bf00fbf5ea4f2d8c190fe4
Alanna <phan.alanna@gmail.com>
2023-11-20 18:36:38 -0500

    add about us page

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  get '/about' => 'about#index'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
